aa2b182065901fd5374ab77c0ef6be9f592c9243
BurakOZN <burakozn@gmail.com>
2020-11-05 21:04:18 +0300

    IsDone type bool olarak değiştirildi.

--- ToDo/Entity/Job.cs
+++ ToDo/Entity/Job.cs
@@ -7,7 +7,7 @@ namespace Entity
         public string Name { get; set; }
         public string Description { get; set; }
         public string UserId { get; set; }
-        public string IsDone { get; set; }
+        public bool IsDone { get; set; }
         public DateTime StartDate { get; set; }
         public JobType JobType { get; set; }
     }
